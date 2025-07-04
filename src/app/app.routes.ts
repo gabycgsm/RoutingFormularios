07dba3a38bd4c28679a87df78fd7414f979fd58e
csaavedra <gabyc_gsm@yahoo.es>
2025-07-04 17:59:44 -0500

    formulario normal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { FormularioNormal } from './formulario-normal/formulario-normal';
+import { FormularioReactivo } from './formulario-reactivo/formulario-reactivo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: FormularioNormal,
+        title: "Formulario NOrmal"
+    },
+    {
+        path:"reactivo",
+        component: FormularioReactivo,
+        title: "Formulario NOrmal"
+    }
+];
--- src/app/formulario-normal/formulario-normal.ts
+++ src/app/formulario-normal/formulario-normal.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-normal',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './formulario-normal.html',
   styleUrl: './formulario-normal.css'
 })
 export class FormularioNormal {
+  nombre: string = '';
+  nombres: string[] = [];
+
+  enviar(){
+    if(this.nombre.trim() ){
+      this.nombres.push(this.nombre.trim())
+      this.nombre = "";
+    }
+    
+  }
 
 }
