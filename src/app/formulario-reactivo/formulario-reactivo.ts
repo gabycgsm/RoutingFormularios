b586644fe5cb118eae711e2648876cb28254971e
csaavedra <gabyc_gsm@yahoo.es>
2025-07-04 18:39:16 -0500

    formulario reactivo

--- src/app/formulario-reactivo/formulario-reactivo.ts
+++ src/app/formulario-reactivo/formulario-reactivo.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario-reactivo.html',
   styleUrl: './formulario-reactivo.css'
 })
 export class FormularioReactivo {
+
+  formulario = new FormGroup({
+    nombre: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  handleSubmit(){
+    alert(
+      this.formulario.value.nombre + ' | ' + this.formulario.value.email
+    );
+  }
 
 }
