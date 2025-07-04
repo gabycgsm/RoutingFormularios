bbc9d4166480f77b4b64f727260a8d5ad5174efb
csaavedra <gabyc_gsm@yahoo.es>
2025-07-04 17:27:17 -0500

    version inicial

---
+++ src/app/formulario-normal/formulario-normal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-normal',
+  imports: [],
+  templateUrl: './formulario-normal.html',
+  styleUrl: './formulario-normal.css'
+})
+export class FormularioNormal {
+
+}
